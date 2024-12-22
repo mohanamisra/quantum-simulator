38f8847497228dd409850edae59d40d140ba0569
Mohana Misra <iammohanamisra@gmail.com>
2024-12-22 17:27:15 +0530

    Quantum wire and display preliminary structure

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -4,6 +4,14 @@ const Display = () => {
     return (
         <section className="display">
             <h2>Simulator</h2>
+            <div className="circuit-area">
+                <div className="circuit-block">
+                    <span className="qubit-name">q[0]</span>
+                    <input className="wire-input"/>
+                    <div className="wire"></div>
+                    <span className="wire-output">X</span>
+                </div>
+            </div>
         </section>
     );
 };
