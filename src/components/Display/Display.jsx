4a6c6909ea145d87e875000b4ead7e2f7e6040af
Mohana Misra <iammohanamisra@gmail.com>
2024-12-22 21:03:19 +0530

    Added not gate functionality for wire 1, abstracted input change into a general function

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -10,6 +10,9 @@ const Display = () => {
         // {"operator":"or", "symbol": "+"},
         // {"operator":"and", "symbol": "."},
     ]);
+    const [wireInput1, setWireInput1] = useState(0)
+    const [wireOutput1, setWireOutput1] = useState(0)
+
     const [gatesList2, setGatesList2] = useState([
         // MOCK DATA TO USE IN DEV
         // {"operator":"and", "symbol": "."},
@@ -18,6 +21,9 @@ const Display = () => {
         // {"operator":"or", "symbol": "+"},
         // {"operator":"and", "symbol": "."},
     ]);
+    const [wireInput2, setWireInput2] = useState(0)
+    const [wireOutput2, setWireOutput2] = useState(0)
+
     const [gatesList3, setGatesList3] = useState([
         // MOCK DATA TO USE IN DEV
         // {"operator":"and", "symbol": "."},
@@ -26,10 +32,23 @@ const Display = () => {
         // {"operator":"or", "symbol": "+"},
         // {"operator":"and", "symbol": "."},
     ]);
+    const [wireInput3, setWireInput3] = useState(0)
+    const [wireOutput3, setWireOutput3] = useState(0)
 
     const handleDragOver = (e) => {
         e.preventDefault();
         console.log("dragging over");
+    }
+
+    const calculateWireOutput = (oldOutput, operator) => {
+        let newOutput;
+        switch(operator) {
+            case "not":
+                newOutput = Number(!oldOutput);
+                setWireOutput1(newOutput);
+                break;
+        }
+        return newOutput;
     }
 
     const handleDrop1 = (e) => {
@@ -37,6 +56,9 @@ const Display = () => {
             let newGate = JSON.parse(e.dataTransfer.getData("text/plain"));
             newGate = {id: gatesList1.length, ...newGate};
             setGatesList1([...gatesList1, newGate]);
+
+            const newOutput = calculateWireOutput(wireOutput1, newGate.operator);
+            setWireOutput1(newOutput);
         }
         else {
             alert("ERROR: A maximum of 5 gates are allowed on the simulator quantum wire at a time.")
@@ -47,6 +69,9 @@ const Display = () => {
             let newGate = JSON.parse(e.dataTransfer.getData("text/plain"));
             newGate = {id: gatesList2.length, ...newGate};
             setGatesList2([...gatesList2, newGate]);
+
+            const newOutput = calculateWireOutput(wireOutput2, newGate.operator);
+            setWireOutput2(newOutput);
         }
         else {
             alert("ERROR: A maximum of 5 gates are allowed on the simulator quantum wire at a time.")
@@ -57,11 +82,31 @@ const Display = () => {
             let newGate = JSON.parse(e.dataTransfer.getData("text/plain"));
             newGate = {id: gatesList3.length, ...newGate};
             setGatesList3([...gatesList3, newGate]);
+
+            const newOutput = calculateWireOutput(wireOutput3, newGate.operator);
+            setWireOutput3(newOutput);
         }
         else {
             alert("ERROR: A maximum of 5 gates are allowed on the simulator quantum wire at a time.")
         }
     }
+
+    const handleWireInputChange = (e, wireNum) => {
+        const value = e.target.value;
+        if (value === '0' || value === '1' || value === '') {
+            switch(wireNum) {
+                case 1:
+                    setWireInput1(value);
+                    break;
+                case 2:
+                    setWireInput2(value);
+                    break;
+                case 3:
+                    setWireInput3(value);
+                    break;
+            }
+        }
+    };
 
     return (
         <section className="display">
@@ -71,13 +116,10 @@ const Display = () => {
                     <span className="qubit-name">q[0]</span>
                     <input
                         className="wire-input"
+                        value = {wireInput1}
                         type="text"
                         maxLength="1"
-                        onInput={(e) => {
-                            if (!['0', '1'].includes(e.target.value)) {
-                                e.target.value = ''; // Clear invalid input
-                            }
-                        }}
+                        onChange={(e)=>handleWireInputChange(e, 1)}
                     />
                     <div className="wire"></div>
                     <div className="wire-gates-area">
@@ -89,21 +131,17 @@ const Display = () => {
                             )
                         })}
                     </div>
-                    <span className="wire-output"></span>
+                    <div className="wire-output">{wireOutput1}</div>
                 </div>
                 <div className="circuit-block" onDragOver={handleDragOver} onDrop={handleDrop2}>
                     <span className="qubit-name">q[0]</span>
                     <input
                         className="wire-input"
+                        value={wireInput2}
                         type="text"
                         maxLength="1"
-                        onInput={(e) => {
-                            if (!['0', '1'].includes(e.target.value)) {
-                                e.target.value = ''; // Clear invalid input
-                            }
-                        }}
+                        onChange={(e)=>handleWireInputChange(e, 2)}
                     />
-
                     <div className="wire"></div>
                     <div className="wire-gates-area">
                         {gatesList2.map((gate, index) => {
@@ -114,19 +152,16 @@ const Display = () => {
                             )
                         })}
                     </div>
-                    <span className="wire-output"></span>
+                    <div className="wire-output">{wireOutput2}</div>
                 </div>
                 <div className="circuit-block" onDragOver={handleDragOver} onDrop={handleDrop3}>
                     <span className="qubit-name">q[0]</span>
                     <input
                         className="wire-input"
+                        value={wireInput3}
                         type="text"
                         maxLength="1"
-                        onInput={(e) => {
-                            if (!['0', '1'].includes(e.target.value)) {
-                                e.target.value = ''; // Clear invalid input
-                            }
-                        }}
+                        onChange={(e)=>handleWireInputChange(e, 3)}
                     />
                     <div className="wire"></div>
                     <div className="wire-gates-area">
@@ -138,7 +173,7 @@ const Display = () => {
                             )
                         })}
                     </div>
-                    <div className="wire-output"></div>
+                    <div className="wire-output">{wireOutput3}</div>
                 </div>
             </div>
         </section>
