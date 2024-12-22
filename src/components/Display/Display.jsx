93e3819fb3df286f1ff264823b63858156e6ca21
Mohana Misra <iammohanamisra@gmail.com>
2024-12-22 20:18:24 +0530

    Wire appending functionality working fine

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -2,7 +2,14 @@ import {useState} from "react";
 import './Display.css'
 
 const Display = () => {
-    const [gatesList, setGatesList] = useState([]);
+    const [gatesList, setGatesList] = useState([
+        // MOCK DATA TO USE IN DEV
+        // {"operator":"and", "symbol": "."},
+        // {"operator":"or", "symbol": "+"},
+        // {"operator":"not", "symbol": "~"},
+        // {"operator":"or", "symbol": "+"},
+        // {"operator":"and", "symbol": "."},
+    ]);
 
     const handleDragOver = (e) => {
         e.preventDefault();
@@ -10,7 +17,13 @@ const Display = () => {
     }
 
     const handleDrop = (e) => {
-        console.log(e.dataTransfer.getData("text/plain"));
+        if(gatesList.length < 5) {
+            const newGate = JSON.parse(e.dataTransfer.getData("text/plain"));
+            setGatesList([...gatesList, newGate]);
+        }
+        else {
+            alert("ERROR: A maximum of 5 gates are allowed on the simulator quantum wire at a time.")
+        }
     }
 
     return (
@@ -21,6 +34,15 @@ const Display = () => {
                     <span className="qubit-name">q[0]</span>
                     <input className="wire-input"/>
                     <div className="wire"></div>
+                    <div className = "wire-gates-area">
+                        {gatesList.map((gate, index)=> {
+                            return(
+                                <div key = {index} className="wire-gate gate">
+                                    <span>{gate.symbol}</span>
+                                </div>
+                            )
+                        })}
+                    </div>
                     <span className="wire-output">X</span>
                 </div>
             </div>
